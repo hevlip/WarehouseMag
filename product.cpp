d48ab6f184eda1151df8362cd4ddc2a55145d16c
hevlip <hiva2718@gmail.com>
2026-04-10 22:57:50 +0300

    first

---
+++ product.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("+------------------+   +------------------+   +------------------+\n");
+    printf("|      URUN        |   |     SIPARIS      |   |      SATIS       |\n");
+    printf("+------------------+   +------------------+   +------------------+\n\n");
+
+    // Men�
+    printf("1 - Urun Islemleri\n");
+    printf("2 - Siparis Islemleri\n");
+    printf("3 - Kaydet ve Cik\n");
+
+    return 0;
+}

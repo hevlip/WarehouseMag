3f8b69cb1de5815cd99f8764e1b17d3539047bfd
hevlip <hiva2718@gmail.com>
2026-04-14 20:36:31 +0300

    Update product.cpp
    Add Swetch case

--- product.cpp
+++ product.cpp
@@ -1,15 +1,35 @@
 #include <stdio.h>
 
 int main() {
+    int secim;
 
     printf("+------------------+   +------------------+   +------------------+\n");
-    printf("|      URUN        |   |     SIPARIS      |   |      SATIS       |\n");
+    printf("|      URUN        |   |     SIPARIS      |   |      satis       |\n");
     printf("+------------------+   +------------------+   +------------------+\n\n");
 
-    // Men�
     printf("1 - Urun Islemleri\n");
     printf("2 - Siparis Islemleri\n");
     printf("3 - Kaydet ve Cik\n");
+    scanf("%d",&secim);  
+	printf("%d ",secim);
+    
+    
+    switch(secim){
+    	case 1:
+    		printf("Urun islemleri");
+    	break;
+    	
+    	case 2:
+    	printf("Siparis islemleri");
+    	break;
+    	
+    	case 3:
+    	printf("Kaydet ve Cik");
+    	break;
+    	
+    	default:
+    		printf("Yanlis secetiniz lutefen menu deki sailar secininz sadece ");
+	}
 
     return 0;
 }
